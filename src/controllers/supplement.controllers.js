06827fc6fdb555708729dede036e95c6a744eca7
codethi <docencia.thiago@gmail.com>
2022-11-23 16:41:22 -0300

    feat: middleware supplement schema valid

--- src/controllers/supplement.controllers.js
+++ src/controllers/supplement.controllers.js
@@ -1,8 +1,7 @@
 export async function createSupplement(req, res) {
+  const supplement = res.locals.supplement;
 
+  
 }
 
-
-export async function findAllSupplement(req, res) {
-
-}
+export async function findAllSupplement(req, res) {}
